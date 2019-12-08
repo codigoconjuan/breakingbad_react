956f6ccb9f975daeeb1eeb35522f1e7e2fa96151
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-08 13:23:18 -0600

    css a la frase

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+
+const ContenedorFrase = styled.div`
+    padding: 3rem;
+    border-radius: .5rem;
+    background-color: #fff;
+    max-width:800px;
+    
+    @media (min-width: 992px) {
+        margin-top: 10rem;
+    }
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight:bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+`;
 
 const Frase = ({frase}) => {
+
     return ( 
-        <div>
+        <ContenedorFrase>
             <h1>{frase.quote} </h1>
             <p>- {frase.author} </p>
-        </div>
+        </ContenedorFrase>
      );
 }
  
