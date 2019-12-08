27a433a13c241bdeeef84aed6a6b42ec7aafda4f
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-08 12:44:50 -0600

    agregando boton para consultar api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+` ;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+
+  const consultarAPI = () => {
+    console.log('consultando...');
+  }
+
   return (
-    <h1>Breaking bad</h1>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
